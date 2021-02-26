9243038e8426c671bb4e72cbafe48bbfc38cbd57
jenkins96 <adrianjnkns@gmail.com>
2021-02-26 01:51:41 +0000

    adjunting interval

--- src/js/component/SecondsCounter.js
+++ src/js/component/SecondsCounter.js
@@ -1,1 +1,19 @@
-import React from "react";
+/*import React from "react";
+
+const SecondsCounter = incrementSeconds() => {
+	let seconds = 0;
+	return (
+        seconds += 1);
+	let counter = setInterval(incrementSeconds, 1000);
+};
+export default SecondsCounter;
+
+//var cancel = setInterval(incrementSeconds, 1000);
+
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    console.log('This will run every second!');
+  }, 1000);
+  return () => clearInterval(interval);
+}, []);*/
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Card from "./Card";
 import CardIcon from "./CardIcon";
-//import SecondsCounter from "./SecondsCounter";
+import SecondsCounter from "./SecondsCounter";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="d-flex justify-content-center">
 			<CardIcon icon="https://cdn.iconscout.com/icon/free/png-256/clock-1605637-1360989.png" />
-			<Card title="0" />
+			<Card title={SecondsCounter} />
 			<Card title="0" />
 			<Card title="0" />
 			<Card title="0" />
