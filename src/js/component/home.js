894b6f1ff734f2941282941b7c8a5b1fb0994868
jenkins96 <adrianjnkns@gmail.com>
2021-03-02 03:09:11 +0000

    done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,19 +1,21 @@
-import React from "react";
-import Card from "./Card";
-import CardIcon from "./CardIcon";
-import SecondsCounter from "./SecondsCounter";
+import React, { useEffect, useState } from "react";
+import { SecondsCounter } from "./SecondsCounter";
 
+let i = 0;
+
+const Clock = () => {
+	const [secs, setTime] = useState(i);
+	useEffect(() => {
+		setInterval(() => setTime(i++), 1000);
+	}, []);
+
+	return <SecondsCounter seconds={secs.toString()} />;
+};
 //create your first component
 export function Home() {
 	return (
-		<div className="d-flex justify-content-center">
-			<CardIcon icon="https://cdn.iconscout.com/icon/free/png-256/clock-1605637-1360989.png" />
-			<Card title={SecondsCounter} />
-			<Card title="0" />
-			<Card title="0" />
-			<Card title="0" />
-			<Card title="0" />
-			<Card title="0" />
+		<div className="text-white p-5">
+			<Clock />
 		</div>
 	);
 }
